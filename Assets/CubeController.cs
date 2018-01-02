8cc137bd349f92fcd4ecbf4b897728e628f383f6
yxx3tch <yxx3tch@gmail.com>
2018-01-02 23:30:07 +0900

    first commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+        // キューブの移動速度
+        private float speed = -0.2f;
+
+        // 消滅位置
+        private float deadLine = -10;
+        
+        // Use this for initialization
+        void Start(){
+        }
+
+        // Update is called once per frame
+        void Update () {
+                // キューブを移動させる
+                transform.Translate (this.speed, 0, 0);
+                
+                // 画面外に出たら破棄する
+                if (transform.position.x < this.deadLine){
+                        Destroy (gameObject);
+                }
+        }
+}
